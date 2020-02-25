f7ad745ae14af7cd9af09e2978e67ea4651f17b2
Kamy <kamy.leeret@uchealth.org>
2020-02-24 18:46:25 -0700

    player guess buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
 
 //There will be four crystals displayed as buttons on the page.
 
-var buttons = ["btn1", "btn2", "btn3", "btn4"];
-
 var playerGuess = [];   // player guess when they click a crystal
 var guessHolder;        //
 var playerGuessTotal;   // add all guesses to get total score
@@ -18,39 +16,84 @@ $(document).ready(function () {
 
     //STEP 1 -- start game:  Trigger Event (ON CLICK) SELECT RANDOM NUMBER 
 
-
     $(".start-game").on("click", function () {
 
-        $("#random-number").text(Math.floor(Math.random() * 100));
-            var chosenNumber = "";  //string to hold random number
-            for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 9.
-                var random = Math.floor((Math.random() * 100));
-                chosenNumber = random + chosenNumber;
-                console.log(random);
+        //create random number as computer guess
+        $("#random-number").text(Math.floor(Math.random() * 10));
+        var random = Math.floor(Math.random() * 10);
+
+        //initiate a loop to generate random numbers 
+        for (var i = 0; i < 9; i++) {
+            console.log(random);
         };//end of random number
+
     });//end of ChosenNumber
 
     //STEP 2 -- user guess:  Get the user's pick
 
     $(".player-total").on("click", function () {
 
-        $("#player-guess").text(Math.floor(Math.random() * 100));
+        $("#btn1").text(Math.floor(Math.random() * 10));
             var playerGuess = "";  //string to hold random number
-            for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 9.
-                var random = Math.floor(Math.random() * 100);
-                playerTotal = random + playerGuess;
-                console.log(random);
-                console.log(playerGuess);
+
+        for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
+            var random = Math.floor(Math.random() * 10);
+            playerTotal = random + playerGuess;
+            console.log("player guess " + playerGuess);
         };//end of random number
+
+        $("#btn2").text(Math.floor(Math.random() * 10));
+        var playerGuess = "";  //string to hold random number
+
+    for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
+        var random = Math.floor(Math.random() * 10);
+        playerTotal = random + playerGuess;
+        console.log("player guess " + playerGuess);
+
+        $("#btn3").text(Math.floor(Math.random() * 10));
+        var playerGuess = "";  //string to hold random number
+
+    for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
+        var random = Math.floor(Math.random() * 10);
+        playerTotal = random + playerGuess;
+        console.log("player guess " + playerGuess);
+    };//end of random number
+
+    $("#btn4").text(Math.floor(Math.random() * 10));
+    var playerGuess = "";  //string to hold random number
+
+for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
+    var random = Math.floor(Math.random() * 10);
+    
+    playerTotal = random + playerGuess;
+    console.log("player guess " + playerGuess);
+};//end of random number
+
+
+    };//end of random number
+
+
+
+
     });//end of ChosenNumber
 
 
-      //  $(".user-guess").on("click", function() {
-                   
-           
-        //     ​console.log("You clicked a button!!");
-    
-    
+
+
+    //  $(".user-guess").on("click", function() {
+
+
+    //     ​console.log("You clicked a button!!");
+
+    // function reset() {
+    //  guessesRemaining = 13;
+    //  wrongGuess = [];
+    //  blanksAndCorrect = [];
+    // Game()
+    //}
+
+    //  keep an array of previous guesses and check ea guess against what the user has alerady guessed
+
     //STEP 3 -- add user guesses: get the user guess total
 
     //STEP 4 -- compare user guess total to random number
