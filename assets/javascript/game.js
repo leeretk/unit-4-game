d4ae95c2c355d3c7e6cea5880902834009f56ac1
Kamy <kamy.leeret@uchealth.org>
2020-02-24 22:20:19 -0700

    adding pseudocode
    Document the process flow

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,108 @@
+//CRYSTALS GAME
+//------------------------------------------------------------------------------------------//    
 
-//There will be four crystals displayed as buttons on the page.
+//Games Variables
+var gameCounter=3;        //count games played
 
+//Game Outcome Variables 
+var gameOutcomes = ["win", "lose"]
+var win = 0;                //when total guess score matches the random number.
+var lose = 0;               //when total guess score does not match the random number.
+var gameOutcomeHolder;      //collects all wins and loses until reload
+
+//Guess Variables 
+var guessCounter = 4;       //count guesses
+
+//Guess Outcome Variables
+
+var gameGuesses = ["correct", "incorrect"]
 var playerGuess = [];   // player guess when they click a crystal
 var guessHolder;        //
 var playerGuessTotal;   // add all guesses to get total score
 
-var win;                //when total guess score matches the random number.
-var lose;               //when total guess score does not match the random number.
-var gamesWon =0;           //adds all games won until reload
-var gamesLost =0;          //adds all games lost until reload
-var gameCounter;        //count games played
-var guessCounter;       //count guesses
+//------------------------------------------------------------------------------------------//
 
-$(document).ready(function () {
+//ensure the code doesn't run until the HTML is finished loading:
+$(document).ready(function () {                     
 
+//------------------------------------------------------------------------------------------//    
     //STEP 1 -- start game:  Trigger Event (ON CLICK) SELECT RANDOM NUMBER 
+        //-- create start game onclick function 
+            //-- add random-number generator function
+            //-- log results for random number
+            //-- log results for game count
 
-    $(".start-game").on("click", function () {
-
-        //create random number as computer guess
-        $("#random-number").text(Math.floor(Math.random() * 10));
-        var random = Math.floor(Math.random() * 10);
-
-        //initiate a loop to generate random numbers 
-        for (var i = 0; i < 9; i++) {
-            console.log(random);
-        };//end of random number
-
+    $("#start-game").on("click", function () {
+        
+        $("#random-number").text(Math.floor(Math.random() * 100)); {     
+        var randomNumber = Math.floor(Math.random() * 100); 
+        console.log("random number " + randomNumber);
+        console.log("game count " + gameCounter++);
+        }; //end of random number
     });//end of ChosenNumber
+//------------------------------------------------------------------------------------------//    
+    //add code here to limit the number of games to 3.  
 
-    //STEP 2 -- user guess:  Get the user's pick
+//------------------------------------------------------------------------------------------//    
+    //STEP 2 -- user guess:  
+        //-- on click event to generate users pick 
+        //-- click btn1, 2, 3, 4
+        //-- connect btn to image
+        //-- when user clicks button 
+            //-generate random number
+            //-log random number to guessHolder
+            //-log guess results
+                    //player guess = randomNumber --> you win!
+                    //player guess <= randomeNumber --> guess again!
+                       
+                        //on click of 2nd btn
+                            //IF.... player guess = randomNumber --> you win!    
+                            //ELSE... player guess <= randomeNumber --> guess again!
+                            
+                        //on click of 3rd btn
+                            //IF.... player guess <= randomeNumber --> guess again!
+                            //ELSE...player guess = randomNumber --> you win!
+                        //on click of 4rd btn
+                            //IF.... player guess <= randomeNumber --> guess again!
+                            //ELSE...player guess = randomNumber --> you win!  
 
-    $(".player-total").on("click", function () {
 
+    if (playerGuess === randomNumber)  //button 1
+
+
+
+        $("#player-total").on("click", function () {
+
+        //button 1
         $("#btn1").text(Math.floor(Math.random() * 10));
-            var playerGuess = "";  //string to hold random number
+        var playerGuess = "";  //string to hold random number
+
+        $("#random-number").text(Math.floor(Math.random() * 100)); {     
+            var randomNumber = Math.floor(Math.random() * 100); 
+            console.log(randomNumber);
+            }; //end of random number
+
+
+     else if { //button 2
+
+        $("#btn2").text(Math.floor(Math.random() * 10));
+        var playerGuess = "";  //string to hold random number
+
+        $("#random-number").text(Math.floor(Math.random() * 100)); {     
+            var randomNumber = Math.floor(Math.random() * 100); 
+            console.log(randomNumber);
+            }; //end of random number
+
+            playerTotal = random + playerGuess;
+            console.log("player guess " + playerGuess);
+
+
+            }
+
+    else if {
+
+        $("#btn3").text(Math.floor(Math.random() * 10));
+        var playerGuess = "";  //string to hold random number
 
         for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
             var random = Math.floor(Math.random() * 10);
@@ -42,37 +110,20 @@ $(document).ready(function () {
             console.log("player guess " + playerGuess);
         };//end of random number
 
-        $("#btn2").text(Math.floor(Math.random() * 10));
+    }    
+
+    else {
+        $("#btn4").text(Math.floor(Math.random() * 10));
         var playerGuess = "";  //string to hold random number
 
-    for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
-        var random = Math.floor(Math.random() * 10);
-        playerTotal = random + playerGuess;
-        console.log("player guess " + playerGuess);
+        for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
+            var random = Math.floor(Math.random() * 10);
 
-        $("#btn3").text(Math.floor(Math.random() * 10));
-        var playerGuess = "";  //string to hold random number
+            playerTotal = random + playerGuess;
+            console.log("player guess " + playerGuess);
+        };//end of random number
 
-    for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
-        var random = Math.floor(Math.random() * 10);
-        playerTotal = random + playerGuess;
-        console.log("player guess " + playerGuess);
-    };//end of random number
-
-    $("#btn4").text(Math.floor(Math.random() * 10));
-    var playerGuess = "";  //string to hold random number
-
-for (var i = 0; i < 9; i++) {                           // initiate a loop to generate random numbers between 0 and 99.
-    var random = Math.floor(Math.random() * 10);
-    
-    playerTotal = random + playerGuess;
-    console.log("player guess " + playerGuess);
-};//end of random number
-
-
-    };//end of random number
-
-
+    }
 
 
     });//end of ChosenNumber
