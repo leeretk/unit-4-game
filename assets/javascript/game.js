efe4043e9944cf43c662d42ef5c3525c1c0f3795
Kamy <kamy.leeret@uchealth.org>
2020-02-25 21:09:02 -0700

    BUTTONS UGH!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,44 @@
 //CRYSTALS GAME
-//------------------------------------------------------------------------------------------//    
 
-//Game Outcome Variables 
-var gameOutcomes = ["win", "lose"]
-var win = 0;                //when total guess score matches the random number.
-var lose = 0;               //when total guess score does not match the random number.
-var gameOutcomeHolder;      //collects all wins and loses until reload
+//-------------------------------------------------------------------------//   
 
-//Guess Outcome Variables
-var gameGuesses = ["correct", "incorrect"]
-var playerGuess = [];   // player guess when they click a crystal
-var guessHolder;        //
-var playerGuessTotal;   // add all guesses to get total score
+//-------------------------------------------------------------------------//
 
-//Games Variables
-var gameCounter=3;        //count games played
 
-//Guess Variables 
-var guessCounter = 4;       //count guesses
+var randomNumber 
 
-//------------------------------------------------------------------------------------------//
-    //ensure the code doesn't run until the HTML is finished loading:
+//ensure the code doesn't run until the HTML is finished loading:
 
 $(document).ready(function () {                     
 
- //-----------------------------------------------------------------------------------------//
+ //-------------------------------------------------------------------------//
     //STEP 1 -- start game:  Trigger Event (ON CLICK) SELECT RANDOM NUMBER 
         //-- create start game onclick function 
             //-- add random-number generator function
             //-- log results for random number
             //-- log results for game count
-
+    
     $("#start-game").on("click", function () {
-        $("#random-number").text(Math.floor(Math.random() * 100)); {     
-        var randomNumber = Math.floor(Math.random() * 100); 
-        console.log("random number " + randomNumber);
-        console.log("game count " + gameCounter++);
-        }; //end of random number
-    });//end of ChosenNumber
- //----------------------------------------------------------------------------------------//    
+
+        $("#random-number").text {
+            var randomNumber = Math.floor(Math.random() * 100) + 1;
+            console.log("Match to this Number: " + randomNumber)
+            alert("random number is " + randomNumber)
+          };//end of random number
+    });//end of Number to Guess
+     
+ //-------------------------------------------------------------------------//    
     //add code here to limit the number of games to 3.  
 
- //--------------------------------------------------------------------------------------//    
-    //STEP 2 -- plaer guess:  
+ //-------------------------------------------------------------------------//   
+     //STEP 2 -- plaer guess:  
         //-- on click event to generate player pick
+
+//    $("#player-guess").on("click", function() {
+  //      alert("you clicked a crystal!")
+    //})    
+
+
         //-- click btn1, 2, 3, 4
         //-- connect btn to image
         //-- when player clicks button 
@@ -96,6 +91,3 @@ $(document).ready(function () {
     //STEP 6 -- If Lose
 
 });//end of script
-
-
-
